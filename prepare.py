5eee930e9304baaecf5e4d8872694b66a1850775
Ken-Alviola <kenalviola@gmail.com>
2021-02-23 14:41:10 -0600

    prepare exercise

---
+++ prepare.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def prep_iris(df):
+    '''
+    Takes a iris dataframe drops species_id and measurement_id,renames species_name to just species and creates dummy variables for species
+    return: single cleaned dataframe
+    '''
+    df.drop_duplicates(inplace=True)
+    dropcols = ['species_id','measurement_id']
+    df.drop(columns=dropcols, inplace=True)
+    df.columns=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
+    dummies = pd.get_dummies(df['species'], drop_first=True)
+    return pd.concat([df, dummies], axis=1)
