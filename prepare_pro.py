99dff346bfd7846a180b4740db1dcf2f81ecce58
Ken-Alviola <kenalviola@gmail.com>
2021-02-25 18:23:46 -0600

    adding Ryans generic split function

--- prepare_pro.py
+++ prepare_pro.py
@@ -32,3 +32,18 @@ def prep_iris(df):
     train_validate, test = train_test_split(df, test_size=0.2, random_state=1349, stratify=df.species)
     train, validate = train_test_split(train_validate, train_size=0.7, random_state=1349, stratify=train_validate.species)
     return train, validate, test
+
+def generic_split(df, stratify_by=None):
+    """
+    Crude train, validate, test split
+    To stratify, send in a column name
+    """
+    
+    if stratify_by == None:
+        train, test = train_test_split(df, test_size=.3, random_state=123)
+        train, validate = train_test_split(df, test_size=.3, random_state=123)
+    else:
+        train, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[stratify_by])
+        train, validate = train_test_split(df, test_size=.3, random_state=123, stratify=train[stratify_by])
+    
+    return train, validate, test
