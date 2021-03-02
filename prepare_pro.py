ab32a777e72a51b488cd4e137c8432938e932460
Ken-Alviola <kenalviola@gmail.com>
2021-03-01 20:29:51 -0600

    initial commit

--- prepare_pro.py
+++ prepare_pro.py
@@ -43,7 +43,7 @@ def generic_split(df, stratify_by=None):
         train, test = train_test_split(df, test_size=.3, random_state=123)
         train, validate = train_test_split(df, test_size=.3, random_state=123)
     else:
-        train, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[stratify_by])
-        train, validate = train_test_split(df, test_size=.3, random_state=123, stratify=train[stratify_by])
+        train_validate, test = train_test_split(df, test_size=.2, random_state=123, stratify=df[stratify_by])
+        train, validate = train_test_split(train_validate, test_size=.3, random_state=123, stratify=train_validate[stratify_by])
     
     return train, validate, test
